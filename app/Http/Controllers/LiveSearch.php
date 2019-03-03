858a50172e626a58339408a74e00f6f476d4f396
Mostafa Magdy <mosafa697@gmail.com>
2019-03-03 15:42:13 +0200

    controller & view added
    created new controller to code in it to keep focus, and when finched will concatenate them

---
+++ app/Http/Controllers/LiveSearch.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class LiveSearch extends Controller
+{
+    function index()
+    {
+     return view('liveSearch');
+    }
+
+    
+}
+?>
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('books','booksController');
+Route::get('/live_search','liveSearch@index');
