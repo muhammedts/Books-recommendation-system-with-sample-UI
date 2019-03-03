089e0dcfbfa5ecd61213e292265a2f760cb8ecbb
muhammedts <mohamedts202@gmail.com>
2019-03-03 15:01:05 +0200

    first commit

---
+++ database/migrations/2019_03_01_155514_create_books_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('book_id');
+            $table->Integer('isbn');
+            $table->String('authors');
+            $table->Integer('original_publication_year');
+            $table->String('title');
+            $table->String('language_code');
+            $table->decimal('average_rating');
+            $table->Integer('work_ratings_count');
+            $table->Integer('work_text_reviews_count');
+            $table->Integer('ratings_1');
+            $table->Integer('ratings_2');
+            $table->Integer('ratings_3');
+            $table->Integer('ratings_4');
+            $table->Integer('ratings_5');
+            $table->String('image_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,55 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="{{ url('/') }}">
+        Books Recommendations system
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+      <span class="navbar-toggler-icon"></span>
+  </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="nav navbar-nav">
+              &nbsp;
+          </ul>
+            <ul class="nav navbar-nav">
+          <li class="nav-item ">
+            <a class="nav-link" href="/home">Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/books">Books</a>
+          </li>
+            </ul>
+          <ul class="navbar-nav ml-auto">
+        <!-- Authentication Links -->
+                      @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+        </ul>
+      </div>
+    </div>
+  </nav>
