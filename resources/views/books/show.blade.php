af5894f390e922c24aad67bc5a4e14110e47792b
muhammedts <mohamedts202@gmail.com>
2019-03-05 02:03:59 +0200

    ratings repair

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -40,30 +40,30 @@
                           <div class="col-xs-12 col-md-6 text-center">
                               <h1 class="rating-num">{{$book->average_rating}}</h1>
                               <div class="rating">
-                                  @if ( {{$book->average_rating}} > 4.0 ){
+                                  @if( $book->average_rating > 4.0 )
                                         <span class="fas fa-star"></span><span class="fas fa-star"></i>
                                         </span><span class="fas fa-star"></span><span class="fas fa-star">
                                         </span><span class="fas fa-star"></span>
-                                    @elseif ( {{$book->average_rating}} > 3.0 || {{$book->average_rating}} <= 4.0 ){
+                                    @elseif( $book->average_rating > 3.0 || $book->average_rating <= 4.0 )
                                         <span class="fas fa-star"></span><span class="fas fa-star"></i>
                                         </span><span class="fas fa-star"></span><span class="fas fa-star">
                                         </span><span class="far fa-star"></span>
-                                    @elseif ( {{$book->average_rating}} > 2.0 || {{$book->average_rating}} <= 3.0){
+                                    @elseif( $book->average_rating > 2.0 || $book->average_rating <= 3.0)
                                         <span class="fas fa-star"></span><span class="fas fa-star"></i>
                                         </span><span class="fas fa-star"></span><span class="far fa-star">
                                         </span><span class="far fa-star"></span>
-                                    @elseif ( {{$book->average_rating}} > 1.0 || {{$book->average_rating}} <= 2.0){
+                                    @elseif( $book->average_rating > 1.0 || $book->average_rating <= 2.0)
                                         <span class="fas fa-star"></span><span class="fas fa-star"></i>
                                         </span><span class="far fa-star"></span><span class="far fa-star">
                                         </span><span class="far fa-star"></span>
-                                    @elseif ( {{$book->average_rating}} > 0.0 || {{$book->average_rating}} <= 1.0){
+                                    @elseif( $book->average_rating > 0.0 || $book->average_rating <= 1.0)
                                         <span class="fas fa-star"></span><span class="far fa-star"></i>
                                         </span><span class="far fa-star"></span><span class="far fa-star">
                                         </span><span class="far fa-star"></span>
-                                    
+                                    @endif
                               </div>
                               <div>
-                                  <span class="fas fa-star"></span>{{$book->work_rating_count}} rate
+                                  <span class="fas fa-star"></span>{{$book->work_ratings_count}} rate
                               </div>
                           </div>
                           <div class="col-xs-12 col-md-6">
@@ -74,10 +74,11 @@
                                   <div class="col-xs-8 col-md-9">
                                       <div class="progress">
                                           @php
-                                          $percentage_5 = ({{$book->rating_5}}/{{$book->work_rating_count}})*100;
+                                          $percentage_5 = ($book->ratings_5/$book->work_ratings_count)*100;
                                           @endphp
+                                          
                                           <div class="progress-bar bg-success" role="progressbar" aria-valuenow="20"
-                                              aria-valuemin="0" aria-valuemax="100" style="width:{{$percentage_5}} ">
+                                              aria-valuemin="0" aria-valuemax="100" style="width:{{$percentage_5}}% ">
                                               <span class="sr-only">{{$percentage_5}}</span>
                                           </div>
                                       </div>
@@ -89,10 +90,10 @@
                                   <div class="col-xs-8 col-md-9">
                                       <div class="progress">
                                             @php
-                                            $percentage_4 = ({{$book->rating_4}}/{{$book->work_rating_count}})*100;
+                                            $percentage_4 = ($book->ratings_4/$book->work_ratings_count)*100;
                                             @endphp
                                           <div class="progress-bar bg-success" role="progressbar" aria-valuenow="20"
-                                              aria-valuemin="0" aria-valuemax="100" style="width: {{$percentage_4}} ">
+                                              aria-valuemin="0" aria-valuemax="100" style="width: {{$percentage_4}}% ">
                                               <span class="sr-only">{{$percentage_4}}</span>
                                           </div>
                                       </div>
@@ -104,10 +105,10 @@
                                   <div class="col-xs-8 col-md-9">
                                       <div class="progress">
                                             @php
-                                            $percentage_3 = ({{$book->rating_3}}/{{$book->work_rating_count}})*100;
+                                            $percentage_3 = ($book->ratings_3/$book->work_ratings_count)*100;
                                             @endphp
                                           <div class="progress-bar bg-info" role="progressbar" aria-valuenow="20"
-                                              aria-valuemin="0" aria-valuemax="100" style="width: {{$percentage_3}} ">
+                                              aria-valuemin="0" aria-valuemax="100" style="width: {{$percentage_3}}% ">
                                               <span class="sr-only">{{$percentage_3}}</span>
                                           </div>
                                       </div>
@@ -119,10 +120,10 @@
                                   <div class="col-xs-8 col-md-9">
                                       <div class="progress">
                                             @php
-                                            $percentage_2 = ({{$book->rating_2}}/{{$book->work_rating_count}})*100;
+                                            $percentage_2 = ($book->ratings_2/$book->work_ratings_count)*100;
                                             @endphp
                                           <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="20"
-                                              aria-valuemin="0" aria-valuemax="100" style="width: {{$percentage_2}}">
+                                              aria-valuemin="0" aria-valuemax="100" style="width: {{$percentage_2}}%">
                                               <span class="sr-only">{{$percentage_2}}</span>
                                           </div>
                                       </div>
@@ -134,10 +135,10 @@
                                   <div class="col-xs-8 col-md-9">
                                       <div class="progress">
                                             @php
-                                            $percentage_1 = ({{$book->rating_1}}/{{$book->work_rating_count}})*100;
+                                            $percentage_1 = ($book->ratings_1/$book->work_ratings_count)*100;
                                             @endphp
                                           <div class="progress-bar bg-danger" role="progressbar" aria-valuenow="80"
-                                              aria-valuemin="0" aria-valuemax="100" style="width:{{$percentage_1}}">
+                                              aria-valuemin="0" aria-valuemax="100" style="width:{{$percentage_1}}%">
                                               <span class="sr-only">{{$percentage_1}}</span>
                                           </div>
                                       </div>
